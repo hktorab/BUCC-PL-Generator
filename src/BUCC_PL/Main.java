ce31d67b99bdd5834e1dfd7e2803b33e3509ee6d
Humayun Kabir <humayunkabirtorab@gmail.com>
2017-11-23 00:02:33 +0600

    Update Main.java

--- src/BUCC_PL/Main.java
+++ src/BUCC_PL/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-        primaryStage.setTitle("BUCC PL GENERATOR v3.0");
+        primaryStage.setTitle("BUCC PL GENERATOR v3.1");
         primaryStage.setScene(new Scene(root, 725  , 615));
         primaryStage.show();
     }
